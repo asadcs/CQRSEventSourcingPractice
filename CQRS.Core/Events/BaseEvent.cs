f5e791c4f717751dca1e050d3ebb9f6677e23b30
Muhammad.fig <Muhammad@fig-api.com>
2023-05-13 03:13:07 +0500

    NewPostCommmand PostCreatedEvent Added

---
+++ CQRS.Core/Events/BaseEvent.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Events
+{
+    public abstract class BaseEvent : Message
+    {
+        protected BaseEvent(string type)
+        {
+            Type = type;
+        }
+
+        public string Type { get; set; }
+        public int Version { get; set; }
+    }
+}
