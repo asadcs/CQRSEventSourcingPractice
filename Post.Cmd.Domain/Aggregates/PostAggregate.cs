50a3bbf25774108985def65b9e2614b71bd58c61
Muhammad.fig <Muhammad@fig-api.com>
2023-05-14 00:19:32 +0500

    EventStore Repo and its handler implemented

---
+++ Post.Cmd.Domain/Aggregates/PostAggregate.cs
@@ -0,0 +1,42 @@
+﻿using CQRS.Core.Domain;
+using Post.Common.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Domain.Aggregates
+{
+    public class PostAggregate : AggregateRoot
+    {
+        private bool _active;
+        private string _author;
+        private readonly Dictionary<Guid, Tuple<string, string>> _comments = new();
+
+        public bool Active { get => _active; set => _active = value; }
+
+        public PostAggregate()
+        {
+        }
+
+        public PostAggregate(Guid id, string author, string message)
+        {
+            RaiseEvent(new PostCreatedEvent
+            {
+                Id = id,
+                Author = author,
+                Message = message,
+                DatePosted = DateTime.Now
+            });
+        }
+
+        public void Apply(PostCreatedEvent @event)
+        {
+            _id = @event.Id;
+            _active = true;
+            _author = @event.Author;
+        }
+    }
+}
